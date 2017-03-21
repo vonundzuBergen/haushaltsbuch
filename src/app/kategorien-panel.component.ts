ea7ba99fd8896cbaeb933994af196118a21f2ad6
matthiaskugel <matthias.kugel@gmx.de>
2017-03-21 13:15:33 +0100

    [ADD] Layout

---
+++ src/app/kategorien-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kategorien-panel',
+  templateUrl: '/app/kategorien-panel.component.html',
+  styleUrls: ['app/kategorien-panel.component.css']
+})
+export class KategorienPanelComponent {
+
+}
